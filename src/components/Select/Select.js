2f5cab2a203c9a0c42a5abed647a0a0cc702d8d3
Ndeye Fatou Diop <ndeye-fatou.diop@berkeley.edu>
2022-11-23 09:00:35 +0100

    select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,59 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
-  return (
-    <select value={value} onChange={onChange}>
+  const visibleSelect = (
+    <VisibleSelect>
+      <div>{displayedValue}</div>
+      <Icon id="chevron-down" size={16} />
+    </VisibleSelect>
+  );
+  const invisibleSelect = (
+    <FunctionalSelect value={value} onChange={onChange}>
       {children}
-    </select>
+    </FunctionalSelect>
+  );
+  return (
+    <SelectWrapper>
+      {invisibleSelect}
+      {visibleSelect}
+    </SelectWrapper>
   );
 };
+
+const SelectWrapper = styled.div`
+  position: relative;
+`;
+
+const VisibleSelect = styled.div`
+  padding: 12px;
+  display: flex;
+  gap: 24px;
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  width: fit-content;
+  border-radius: 8px;
+
+  &::hover {
+    color: ${COLORS.black};
+  }
+
+  &:focus {
+    outline: 2px solid -webkit-focus-ring-color;
+  }
+`;
+
+const FunctionalSelect = styled.select`
+  padding: 12px;
+  padding-right: 48px;
+  position: absolute;
+  opacity: 0;
+`;
 
 export default Select;
